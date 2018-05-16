69d49bb5d2d4453ed6480402ec0b0aa303c96967
heming6666 <1040023934@qq.com>
2018-05-16 12:16:44 +0800

    集成首页与登录页面

--- src/permission.js
+++ src/permission.js
@@ -5,7 +5,7 @@ import 'nprogress/nprogress.css'// Progress 进度条样式
 import { Message } from 'element-ui'
 import { getToken } from '@/utils/auth' // 验权
 
-const whiteList = ['/login', '/authredirect'] // 不重定向白名单
+const whiteList = ['/home', '/login', '/authredirect'] // 不重定向白名单
 router.beforeEach((to, from, next) => {
   NProgress.start()
   if (getToken()) {
@@ -33,7 +33,7 @@ router.beforeEach((to, from, next) => {
     if (whiteList.indexOf(to.path) !== -1) {
       next()
     } else {
-      next('/login')
+      next('/home')
       NProgress.done()
     }
   }
